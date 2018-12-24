da2d817e77d0c4523ba16f020ffdc818e97e56eb
umur <umurcg@gmail.com>
2018-12-24 16:25:35 +0300

    basic logic of grid system

---
+++ Assets/LevelModule.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelModule : MonoBehaviour
+{
+
+    public int numberOfRow=1;
+    public int numberOfColumn=1;
+    public int numberOfDepth=1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateScale() {
+        transform.localScale = new Vector3(numberOfColumn, numberOfDepth, numberOfRow);
+    }
+}
+
+
+[CustomEditor(typeof(LevelModule))]
+public class LevelModuleEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelModule myScript = (LevelModule)target;
+        if (GUILayout.Button("Update Scale"))
+        {
+            myScript.updateScale();
+        }
+    }
+}
--- Assets/SurfaceGrid.cs
+++ Assets/SurfaceGrid.cs
@@ -6,13 +6,84 @@ public class SurfaceGrid : MonoBehaviour
 {
 
     public int numberOfCell = 100;
+    public GameObject gridTestExample;
 
+    float unitCellSize;
+
+    bool[,] filledCells;
+    Renderer rend;
     
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        rend=GetComponent<Renderer>();
+
+        var surfaceEdgeSize = rend.bounds.max.x - rend.bounds.min.x;
+        unitCellSize = surfaceEdgeSize / numberOfCell;
+
+        filledCells = new bool[numberOfCell,numberOfCell];
         
+    }
+
+    private void Start()
+    {
+      //Test
+      for(int i = 0; i < 10; i++)
+        {
+            print(findPositionForObjectGroup(3, 2));
+        }
+       
+    }
+
+    Vector3 findPositionForObjectGroup(int numberOfRow,int numberOfColumn)
+    {
+        int leftTry= 100;
+        while (leftTry > 0)
+        {
+            leftTry -= 1;
+
+            //First find random cell in grid
+            var randomRow = Random.Range(0, numberOfCell-numberOfRow);
+            var randomColumn = Random.Range(0, numberOfCell-numberOfColumn);
+            
+
+            //Check weather or not all cells are avaible
+            for(int r = 0; r < numberOfRow;r++)
+            {
+                for(int c = 0; c < numberOfColumn; c++)
+                {
+                    if (filledCells[randomRow + r, randomColumn + c])
+                        continue;
+                }
+            }
+
+            var startPosition = rend.bounds.min;
+
+            //If code reaches it means cells are avaible
+            //Find middle position of cells and fill cells
+            Vector3 positionSum = Vector3.zero;
+
+            for (int r = 0; r < numberOfRow; r++)
+            {
+                for (int c = 0; c < numberOfColumn; c++)
+                {
+                    var row = randomRow + r;
+                    var column = randomColumn + c;
+                    filledCells[row,column] = true;
+                    positionSum += startPosition + new Vector3(row * unitCellSize + unitCellSize / 2, column * unitCellSize + unitCellSize / 2, 0);
+                        
+                }
+            }
+
+            positionSum /= (numberOfRow * numberOfColumn);
+            return positionSum;
+
+
+           
+        }
+
+        return Vector3.zero ;
     }
 
     // Update is called once per frame
